b59ef684ef52976fc242542be900e6487bbc2228
Michael <michaelgiraldo320@gmail.com>
2024-11-16 19:02:26 +0100

    cambios header,añadido de footer con bton arriba y enlace a git

--- src/js/app.js
+++ src/js/app.js
@@ -66,47 +66,82 @@
 
 
 
-// Creo el contenedor div donde se pondra todo lo de busqueda 
-const containerbuscador = document.createElement("div");
-containerbuscador.setAttribute("class", "buscador-container");
+function crearHeader (){
+    // busco el header y le añado una id header 
+    const header = document.querySelector("header");
+    header.setAttribute("id", "header");
 
-// creo el contenedor del div de input 
-const containersearch = document.createElement("div");
-containersearch.setAttribute("class", "search-container");
+    //titulo pokemon
+    const tituloPoke = document.createElement("h1");
+    tituloPoke.textContent = "Pokedex Kanto";
 
-// busco el header y le añado una id header 
-const header = document.querySelector("header");
-header.setAttribute("id", "header");
+    //titulo buscador
 
-// creo el input donde se ingresaran los nombre de los pokemons 
-const searchInput = document.createElement("input");
-searchInput.setAttribute("type", "text");
-searchInput.setAttribute("id", "searchInput");
-searchInput.setAttribute("placeholder", "Buscar Pokémon por nombre");
+    const tituloSearch = document.createElement("label");
+    tituloSearch.setAttribute("for","searchInput");
+    tituloSearch.textContent = "BUSCAR POKÉMON";
 
-// Creo el botón de búsqueda
-const searchButton = document.createElement("button");
-searchButton.setAttribute("type", "button");
-searchButton.setAttribute("id", "searchButton");
+    // Creo el contenedor div donde se pondra todo lo de busqueda 
+    const containerbuscador = document.createElement("div");
+    containerbuscador.setAttribute("class", "buscador-container");
 
-// Creo la img y le añado los atributos que contienen la imagen y la clase 
-const lupaIcon = document.createElement("img");
-lupaIcon.setAttribute("src", "./src/img/icons/lupa.png");
-lupaIcon.setAttribute("class", "lupa-icon");
+    // creo el contenedor del div de input 
+    const containersearch = document.createElement("div");
+    containersearch.setAttribute("class", "search-container");
 
-// hago que sea hijo de del boton 
-searchButton.appendChild(lupaIcon);
+    // creo el input donde se ingresaran los nombre de los pokemons 
+    const searchInput = document.createElement("input");
+    searchInput.setAttribute("type", "text");
+    searchInput.setAttribute("id", "searchInput");
+    searchInput.setAttribute("placeholder", "Buscar Pokémon por nombre");
 
-// Añado el input y el botón al contenedor
-containersearch.appendChild(searchInput);
-containersearch.appendChild(searchButton);
+    // Creo el botón de búsqueda
+    const searchButton = document.createElement("button");
+    searchButton.setAttribute("type", "button");
+    searchButton.setAttribute("id", "searchButton");
 
-// Añado el container-search al buscador-container
-containerbuscador.appendChild(containersearch);
+    // Creo la img y le añado los atributos que contienen la imagen y la clase 
+    const lupaIcon = document.createElement("img");
+    lupaIcon.setAttribute("src", "./src/img/icons/lupa.png");
+    lupaIcon.setAttribute("class", "lupa-icon");
 
-// hago containerbuscador donde se contiene todo como hijo del header 
-header.appendChild(containerbuscador);
+    // hago que sea hijo de del boton 
+    searchButton.appendChild(lupaIcon);
 
+    // Añado el input y el botón al contenedor
+    containersearch.appendChild(searchInput);
+    containersearch.appendChild(searchButton);
+
+    // Añado el container-search al buscador-container
+    containerbuscador.appendChild(containersearch);
+
+
+
+    //titulo pokemon
+    header.appendChild(tituloPoke);
+    //titulo busqueda
+    header.appendChild(tituloSearch);
+    // hago containerbuscador donde se contiene todo como hijo del header 
+    header.appendChild(containerbuscador);
+
+
+    // Evento para escribir y buscar 
+    searchInput.addEventListener('input', function() {
+        buscarPokemon(searchInput);
+    });
+
+    // Evento de clic en el botón para realizar la búsqueda
+    searchButton.addEventListener('click', function() {
+        buscarPokemon(searchInput);
+    });
+
+    // Evento para detectar la tecla Enter 
+    searchInput.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            buscarPokemon(searchInput);
+        }
+    });
+}
 
 function buscarPokemon(searchInput) {
     const pokemonBusqueda = searchInput.value.toLowerCase();
@@ -124,21 +159,61 @@ function buscarPokemon(searchInput) {
     }
 }
 
-// Evento para escribir y buscar 
-searchInput.addEventListener('input', function() {
-    buscarPokemon(searchInput);
-});
 
-// Evento de clic en el botón para realizar la búsqueda
-searchButton.addEventListener('click', function() {
-    buscarPokemon(searchInput);
-});
 
-// Evento para detectar la tecla Enter 
-searchInput.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        buscarPokemon(searchInput);
+
+function crearFooter (){
+    //Boton de Volver Arriba 
+
+    //footer id 
+    const footer = document.querySelector("footer");
+    footer.setAttribute("id", "footer");
+
+    //contenedor div de boton 
+    const divArriba = document.createElement("div");
+    divArriba.setAttribute("class","go-top");
+
+
+    //crear boton
+    const botonArriba = document.createElement("button");
+    botonArriba.setAttribute("type","button");
+    botonArriba.setAttribute("id","arriba");
+
+    //crear span 
+    const spanArriba = document.createElement("span");
+    //creacion de id span por si hay que editar algo
+    spanArriba.setAttribute("id","spanArriba");
+    //contenido span
+    spanArriba.textContent = "Volver arriba";
+
+    //separacion del main con footer 
+
+    const hr = document.createElement("hr");
+
+    //icono git
+    const gitIcon = document.createElement("img");
+    gitIcon.setAttribute("src", "./src/img/icons/logotipo-de-github.png");
+    gitIcon.setAttribute("id", "gitIcon");
+
+
+    botonArriba.appendChild(spanArriba); // span dentro del boton
+    divArriba.appendChild(botonArriba); // el boton dentro del div
+    footer.appendChild(hr); //hr dentro del footer
+    hr.appendChild(gitIcon);
+    footer.appendChild(divArriba); //el div dentro del footer
+
+    //evento que nos redirige arriba de la pagina 
+    botonArriba.addEventListener("click" ,function()  {
+    window.scrollTo({
+        top: 0,
+    });
+    });
+
+    gitIcon.addEventListener("click", function() {
+        window.location.href = 'https://github.com/Luis0sorio/Pokedex-Pokemon';
+    })
     }
-});
 
 
+    crearHeader();
+    crearFooter();
