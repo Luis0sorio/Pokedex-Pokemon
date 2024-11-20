daea14d9db88f89a0b3126d1599c9b90aaf68033
Luis0sorio <luiseosorio96@gmail.com>
2024-11-20 04:25:37 +0100

    final pokedex

--- src/js/aniadirPok.js
+++ src/js/aniadirPok.js
@@ -1,161 +1,144 @@
-    let body= document.body;//hago el 
-    //div para el formulario
-    let divFormulario=document.createElement('div');
-    divFormulario.setAttribute("id","divFormulario");
+let body = document.body;
+let divFormulario = document.createElement("div"); //hago el div para el formulario
+divFormulario.setAttribute("id", "divFormulario");
 
+let main = document.querySelector("main");
 
-    let main = document.querySelector("main");
+//acceder al header
+const header = document.querySelector("header");
+header.setAttribute("id", "header");
 
-    
+//definimos un titulo
+let titulo = document.createElement("h1");
+titulo.textContent = "Crear Pokémon";
 
-    //acceder al header
-    const header = document.querySelector("header");
-    header.setAttribute("id","header");
+//div del titulo
+const divTitulo = document.createElement("div");
+divTitulo.setAttribute("id", "divTitulo");
 
+//creamos el formulario
+let form = document.createElement("form");
+form.method = "GET";
+form.action = "";
+form.id = "formulario";
 
-    //titulo 
-    let titulo = document.createElement('h1');
-    titulo.textContent="Crear Pokémon";
+//creamos los campos para los datos
+//div para el nombre del pokemon
+let divInputNombre = document.createElement("div");
+divInputNombre.setAttribute("id", "divInputNombre");
+let inputNombre = document.createElement("input");
+inputNombre.type = "text";
+inputNombre.id = "nombre";
+inputNombre.name = "nombre";
+inputNombre.placeholder = "Nombre del pokémon";
+inputNombre.required = true; //campo obligatorio
+divInputNombre.appendChild(inputNombre);
 
-    //div titulo
-    const divTitulo = document.createElement("div");
-    divTitulo.setAttribute("id","divTitulo");
+//div para el tipo del pokemon
+let divInputTipo = document.createElement("div");
+divInputTipo.setAttribute("id", "divInputTipo");
+let inputTipo = document.createElement("input");
+inputTipo.type = "text";
+inputTipo.id = "tipo";
+inputTipo.name = "tipo";
+inputTipo.placeholder = "Tipo del Pokémon";
+inputTipo.required = true; //campo obligatorio
+divInputTipo.appendChild(inputTipo);
 
+//boton de enviar
+let div_bton = document.createElement("div");
+div_bton.setAttribute("id", "div_bton");
+let button = document.createElement("input");
+button.id = "btn_env";
+button.type = "submit";
+button.value = "Crear Pokémon";
+div_bton.appendChild(button);
 
+//texto-nombres
+let div_nombre = document.createElement("div");
+let nombre = document.createElement("p");
+nombre.textContent = "Nombre: ";
+div_nombre.appendChild(nombre);
 
+let div_tipo = document.createElement("div");
+let tipo = document.createElement("p");
+tipo.textContent = "Tipo: ";
+div_tipo.appendChild(tipo);
 
-    //creamos el formulario 
-    let form=document.createElement('form');
-    form.method="GET";
-    form.action="";
-    form.id="formulario";
+//boton de volver atrás
+let atras = document.createElement("input");
+atras.type = "button";
+atras.id = "BotonAtras";
+atras.value = "Volver Atrás";
 
-    //creamos los campos para los datos
-    //nombre-pokemon
-    let divInputNombre=document.createElement('div');
-    divInputNombre.setAttribute("id","divInputNombre");
-    let inputNombre= document.createElement('input');
-    inputNombre.type="text";
-    inputNombre.id="nombre";
-    inputNombre.name="nombre";
-    inputNombre.placeholder = "Nombre del pokémon";
-    inputNombre.required=true;//campo obligatorio
-    divInputNombre.appendChild(inputNombre);
+//evento para volver atrás
+atras.addEventListener("click", function (event) {
+  window.location.href = "../../index.html";
+});
 
-    //tipo-pokemon
-    let divInputTipo=document.createElement('div');
-    divInputTipo.setAttribute("id","divInputTipo");
-    let inputTipo= document.createElement('input');
-    inputTipo.type="text";
-    inputTipo.id="tipo";
-    inputTipo.name="tipo";
-    inputTipo.placeholder = "Tipo del Pokémon";
-    inputTipo.required=true;//campo obligatorio
-    divInputTipo.appendChild(inputTipo);
+//aqui añadimos las cosas
+//añadimos el campo nombre
+form.appendChild(div_nombre);
+form.appendChild(divInputNombre);
 
-    //boton enviar
-    let div_bton=document.createElement('div');
-    div_bton.setAttribute("id","div_bton");
-    let button=document.createElement('input');
-    button.id = "btn_env";
-    button.type="submit";
-    button.value="Crear Pokémon";
-    div_bton.appendChild(button);
+//campo tipo
+form.appendChild(div_tipo);
+form.appendChild(divInputTipo);
 
-    //let br1=document.createElement('br');
-    let br2=document.createElement('br');
-    
-    //texto-nombres
-    let div_nombre=document.createElement('div');
-    let nombre=document.createElement('p');
-    nombre.textContent="Nombre: ";
-    div_nombre.appendChild(nombre);
+//boton de enviar
+form.appendChild(button);
+//añadimos el formulario al div donde va tdo
+divFormulario.appendChild(form);
 
-    let div_tipo=document.createElement('div');
-    let tipo=document.createElement('p');
-    tipo.textContent="Tipo: ";
-    div_tipo.appendChild(tipo);
+//hacemos que el h1 sea hijo del divTitulo
+divTitulo.appendChild(titulo);
+//hacemos que el divTitulo sea hijo del header
+header.appendChild(divTitulo);
+// definimos nodos hijos
+main.appendChild(divFormulario);
+form.appendChild(atras);
 
-    //boton de volver atrás
-    //let div_btonAtras=document.createElement('div');
-    //div_btonAtras.setAttribute("id","div_btonAtras");
-    let atras=document.createElement('input');
-    atras.type="button";
-    atras.id="BotonAtras";
-    atras.value="Volver Atrás";
-   // div_btonAtras.appendChild(atras);
-    //evento para volver atrás
-    atras.addEventListener("click",function(event) { 
-        window.location.href="../../index.html";
-    });
-
-    //aqui añadimos las cosas
-    //añadimos el campo nombre
-    form.appendChild(div_nombre);
-    form.appendChild(divInputNombre);
-    
-    //campo tipo
-    form.appendChild(div_tipo);
-    form.appendChild(divInputTipo);
-
-    //boton de enviar
-    form.appendChild(button);
-    //añadimos el formulario al div donde va tdo
-    divFormulario.appendChild(form);
-    
-    //hacemos que el h1 sea hijo del divTitulo
-    divTitulo.appendChild(titulo);
-    //hacemos que el divTitulo sea hijo del header
-    header.appendChild(divTitulo);
-
-//añadimos el titulo y el div al body 
-//    body.appendChild(titulo);
-
-    main.appendChild(divFormulario);
-  //  body.appendChild(divFormulario);
-    form.appendChild(atras);
-
-    //hasta aqui -->creacion de formulario
-    //gestion de los datos, al enviar el formulario
-    function aniadirP() {
-        //bajo los pokemons
-        let pokemons=JSON.parse(localStorage.getItem('pokemitos'));
-        let datos=location.search;//obtenemos los datos despues del ?
-            if (datos) {//si hay datos
-                let datosJuntos=datos.split("&");//separo,para obtener los valores
-                let nombre=datosJuntos[0].split("=");
-                let tipo=datosJuntos[1].split("=");
-                let nombreFinal=nombre[1];
-                let tipoFinal=tipo[1];
-                //al largo del array le sumamos 1 
-                let idd=pokemons.length+1;
-                //creo un objeto cn los datos
-                let nuevo={
-                    id:idd,
-                    nombre:nombreFinal,
-                    tipos:[tipoFinal],
-                    nuevo:true
-                }
-                //lo añado al array
-                pokemons.push(nuevo);
-                //lo subo 
-                localStorage.setItem('pokemitos',JSON.stringify(pokemons));
-                inputNombre.value = "";
-                inputTipo.value = "";
-            }
-    }
-    aniadirP();
-    //aqui n me funciona bien la creacion de la carta
-
+//hasta aqui -->creacion de formulario
+//funcion que gestiona los datos del formulario (nuevo pokemon)
+function aniadirP() {
+  //accdemos a los pokemon
+  let pokemons = JSON.parse(localStorage.getItem("pokemitos"));
+  let datos = location.search; //obtenemos los datos despues del ?
+  if (datos) {
+    //si hay datos separo, para obtener los valores.
+    let datosJuntos = datos.split("&");
+    //separamos las posiciones del array de datos, obtenemos el valor
+    let nombre = datosJuntos[0].split("=");
+    let tipo = datosJuntos[1].split("=");
+    // nos quedamos con el valor restante y lo guardamos en una variable nueva
+    let nombreFinal = nombre[1];
+    let tipoFinal = tipo[1];
+    //al largo del array le sumamos 1
+    let idPokemon = pokemons.length + 1;
+    //creo un objeto con los datos
+    let nuevo = {
+      id: idPokemon,
+      nombre: nombreFinal,
+      tipos: [tipoFinal],
+      nuevo: true, // identificamos al nuevo pokemon
+    };
+    //lo añado al array
+    pokemons.push(nuevo);
+    //lo subo
+    localStorage.setItem("pokemitos", JSON.stringify(pokemons));
+    inputNombre.value = "";
+    inputTipo.value = "";
+  }
+}
+aniadirP();
 
 //accedo al head
-const head = document.querySelector("head");    
+const head = document.querySelector("head");
 
 //crear link para los estilos : style.css
-
 const linkStyle = document.createElement("link");
-linkStyle.setAttribute("rel","stylesheet");
-linkStyle.setAttribute("href","../css/style2.css");
+linkStyle.setAttribute("rel", "stylesheet");
+linkStyle.setAttribute("href", "../css/style2.css");
 
-//hago que los estilos sea hijo del head 
+//hago que los estilos sea hijo del head
 head.appendChild(linkStyle);
